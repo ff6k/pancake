dbe98f0e53f9080def66dc0aa1d1123556af0aab
DESKTOP-JP1MEGQ\Wang <brzden32@gmail.com>
2021-05-01 12:08:18 -0700

    first commit

---
+++ src/components/layout/Page.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Helmet } from 'react-helmet-async'
+import { useLocation } from 'react-router'
+import { customMeta, DEFAULT_META } from 'config/constants/meta'
+import { usePriceCakeBusd } from 'state/hooks'
+import Container from './Container'
+
+const StyledPage = styled(Container)`
+  min-height: calc(100vh - 64px);
+  padding-top: 16px;
+  padding-bottom: 16px;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    padding-top: 24px;
+    padding-bottom: 24px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    padding-top: 32px;
+    padding-bottom: 32px;
+  }
+`
+
+const Background = styled.div`
+  background-image: url('/images/earth-mars.jpg');
+  background-repeat: no-repeat;
+  background-position: cover;
+  background-attachment: fixed;
+  background-size: 100Vw 100vh;
+`
+
+const PageMeta = () => {
+  const { pathname } = useLocation()
+  const cakePriceUsd = usePriceCakeBusd()
+  const cakePriceUsdDisplay = cakePriceUsd.eq(0)
+    ? ''
+    : `$${cakePriceUsd.toNumber().toLocaleString(undefined, {
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3,
+      })}`
+  const pageMeta = customMeta[pathname] || {}
+  const { title, description, image } = { ...DEFAULT_META, ...pageMeta }
+  const pageTitle = cakePriceUsdDisplay ? [title, cakePriceUsdDisplay].join(' - ') : title
+
+  return (
+    <Helmet>
+      <title>{pageTitle}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+    </Helmet>
+  )
+}
+
+const Page: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children, ...props }) => {
+  return (
+    <Background>
+      <PageMeta />
+      <StyledPage {...props}>{children}</StyledPage>
+    </Background>
+  )
+}
+
+export default Page
---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { ThemeProvider as SCThemeProvider } from 'styled-components'
+import { light, dark } from '@pancakeswap-libs/uikit'
+
+const CACHE_KEY = 'IS_DARK'
+
+const ThemeContext = React.createContext({ isDark: null, toggleTheme: () => null })
+
+const ThemeContextProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(() => {
+    const isDarkUserSetting = localStorage.getItem(CACHE_KEY)
+    return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : true
+  })
+
+  const toggleTheme = () => {
+    setIsDark((prevState) => {
+      localStorage.setItem(CACHE_KEY, JSON.stringify(!prevState))
+      return !prevState
+    })
+  }
+
+
+
+  const tempDark = { ...dark, colors: { ...dark.colors, textSubtle: "#ff8800", backgroundAlt: "#000", secondary: "#ff8800", text: "#ff8800" }, card: { ...dark.card, background: "#000" } }
+  console.log('====================================', dark)
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      <SCThemeProvider theme={isDark ? tempDark : light}>{children}</SCThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+export { ThemeContext, ThemeContextProvider }
